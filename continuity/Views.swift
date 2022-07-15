dd85abfaad343fdd0dbf1866c6e00179dfca117b
Lesyk <vlesik34@gmail.com>
2022-07-15 11:55:03 +0200

    Views view

--- continuity/Views.swift
+++ continuity/Views.swift
@@ -5,4 +5,27 @@
 //  Created by 1 on 7/12/22.
 //
 
-import Foundation
+import SwiftUI
+import Combine
+
+
+class Views {
+    struct YourCustomModifier: View {
+        
+        var name: String
+        
+        var body: some View {
+            Text(name)
+                .font(.system(size: 20, weight: .heavy, design: .monospaced))
+                .fontWeight(.heavy)
+                .foregroundColor(.white)
+                .scaleEffect(2)
+                .padding(40)
+                .rotation3DEffect(.degrees(35), axis: (x:4 , y:0, z:0))
+        }
+    }
+}
+
+
+
+
